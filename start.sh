455f9a8ec61d4f56948bf79a6317a1a19f50876d
serafi-nebot-uib <serafi.nebot1@estudiant.uib.cat>
2025-01-06 18:07:17 +0100

    cleanup

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ${HOME}/tetris-m68k-server/.venv/bin/activate
-pip install -r ${HOME}/tetris-m68k-server/requirements.txt
-kill -9 $(cat ${HOME}/tetris-m68k-server/server.pid)
-# TODO: server.log doesn't show anything
-${HOME}/tetris-m68k-server/server.py >> ${HOME}/tetris-m68k-server/server.log 2>&1 & echo $! > ${HOME}/tetris-m68k-server/server.pid
